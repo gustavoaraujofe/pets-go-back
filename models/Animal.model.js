4a658cbae97035b08e4a193ef1aeddeeb6f02568
Alissa Seixas <alissaps@gmail.com>
2021-12-20 12:45:15 -0300

    change animalSchema string

--- models/Animal.model.js
+++ models/Animal.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const animalSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
-  age: { type: Number, required: true },
+  age: { type: String, required: true },
   breed: { type: String, required: true },
-  weight: Number,
+  weight: String,
   gender: { type: String, required: true, trim: true },
   imageUrl: {
     type: String,
